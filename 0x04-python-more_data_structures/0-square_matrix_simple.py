468c000d7a74e853459419e0919a4ae8c33781e5
BrianNjugush <njugunabrian95@gmail.com>
2022-09-29 03:29:31 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    """
-    Square each element of matrix
-    Args:
-        matrix - a 2d lists
-    """
-    if matrix is None:
-        return None
-    return [
-        [x**2 for x in row] for row in matrix
-    ]
-
-
-if __name__ == '__main__':
-    s = square_matrix_simple([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ])
-    print(s)
+    Newsquare = []
+    i = 0
+    for x in matrix:
+        Newsquare.append([])
+        for y in matrix[i]:
+            Newsquare[i].append(y**2)
+        i += 1
+    return (Newsquare)
